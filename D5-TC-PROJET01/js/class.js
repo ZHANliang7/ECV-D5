978d2757baa8a1ba16005ee0580c3c9751e67d67
ZHANLIANG <lucas_zhan@yahoo.com>
2019-10-07 19:19:46 +0200

    1 step

---
+++ D5-TC-PROJET01/js/class.js
@@ -0,0 +1,8 @@
+$(function(){
+    var url = window.location.toString();
+    var id = url.split(“#”)[1];
+   if(id){
+      var t = $(“#”+id).offset().top;
+      $(window).scrollTop(t);
+   }
+});
